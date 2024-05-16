60f030dbcd731d41f13985fa0cd640a12ab1eb18
RaionaAleksander <93260780+RaionaAleksander@users.noreply.github.com>
2024-05-16 14:27:47 +0300

    Update apiparingud.js
    Muutke oma frontend rakenduse HTML ja JavaScript GitHub'is asuvas koodis ümber järgnev sisu:
    Muutke ära API asukohad/lingid. localhost:port asemel reaalne Azure'sse üles seatud mikroteenuse API'de aadressid (ilma portideta, Azure kasutab https porti 443)
    Kindlasti peab API linkides http: asendama https:'ga, kuna Azure nõuab et Azure Service tüüpi takendusi kasutatakse üle HTTPS ühenduse.

--- apiparingud.js
+++ apiparingud.js
@@ -100,15 +100,15 @@ async function handleFormSubmit(event) {
 }
 
 async function listiraamatud() {
-	const responseData = await getDataAsJson("http://localhost:5000/raamatud");
+	const responseData = await getDataAsJson("https://projectpraks12hs9raamatud.azurewebsites.net/raamatud");
 
     const resultElement = document.getElementById("raamatud_result");
 
     resultElement.innerHTML = ""
 
     for (var raamat of responseData.raamatud){
-        resultElement.innerHTML += '<a href="http://localhost:5000/raamatud/'+raamat+'"  download="'+raamat+'.txt" >' +raamat+".txt</a> " +
-                '<a href="#" onclick="deleteObject(\'http://localhost:5000/raamatud/'+raamat+'\')" > [kustuta]</a>' +
+        resultElement.innerHTML += '<a href="https://projectpraks12hs9raamatud.azurewebsites.net/raamatud/'+raamat+'"  download="'+raamat+'.txt" >' +raamat+".txt</a> " +
+                '<a href="#" onclick="deleteObject(\'https://projectpraks12hs9raamatud.azurewebsites.net/raamatud/'+raamat+'\')" > [kustuta]</a>' +
                 "<br />";
     }
 }
